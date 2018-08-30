4811fe6479b059c6f6f97aedb5892d150bc7ea28
HeyItsCptnFalcon <cptnfalcon92@gmail.com>
2018-08-30 16:43:50 -0700

    updated User js pages

---
+++ src/components/user_navbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+
+class UserNav extends Component{
+    state ={
+
+    }
+
+
+    render(){
+        return(
+            <div className = 'container-fluid'>
+                <h1>This will show the Avatar and Settings</h1>
+            </div>
+        )
+    }
+    
+    
+    
+}
--- src/components/userspage.js
+++ src/components/userspage.js
@@ -1,24 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import user_navbar from './user_navbar';
+// import user_sortbar from'./user_sortbar';
+// import user_content from './user_content';
+import ReactPlayer from 'react-player';
 
 
-class UsersPage extends Component{
-    state={
 
-    }
-    render() {
-        return (
-          <div className='container'>
-            <div className="row">
-                <h1>Hey There</h1>
-            </div>
+
+
+class UsersPage extends Component {
+  state = {
+    newurl:[],  
+    urls: [],
+
+  }
+
+
+  NewUrlLink = url =>{
+    let newLink = [...this.state.urls, url]
+    this.setState({
+      urls: newLink
+    })
+  }
+
+
+
+
+
+
+
+  render() {
+    return (
+      <div className='container'>
+        {this.state.urls.map(url => <ReactPlayer url={url} />)}
+        {/* <form 
+        >
+          <div className='form-group'>
+            <label for="userUrl">Paste the URL below</label>
+            <input type="url" value={this.state.urls} className="form-control" id='userUrl' placeholder='URL' />
           </div>
-        );
-      }
+          <button type='submit' className="btn btn-dark">Submit</button>
+        </form> */}
+      </div>
+    );
+  }
 }
 
 
-    
+
 
 
 
