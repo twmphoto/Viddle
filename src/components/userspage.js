60dbce31481d6b014dccb20ea837ebc35e862067
HeyItsCptnFalcon <cptnfalcon92@gmail.com>
2018-08-30 10:55:37 -0700

    Unsure

--- src/components/userspage.js
+++ src/components/userspage.js
@@ -1,25 +1,4 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-
-
-class Userspage extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-export default Userspage;
-=======
 import logo from './logo.svg';
 
 
@@ -46,4 +25,3 @@ class UsersPage extends Component{
 
 
 export default UsersPage;
->>>>>>> 726aeca17373fc599b81344a236deb36871bd603
