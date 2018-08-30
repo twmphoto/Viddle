726aeca17373fc599b81344a236deb36871bd603
John Chu <twmphoto@icloud.com>
2018-08-29 17:43:53 -0700

    2nd update

---
+++ src/components/userspage.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+
+class UsersPage extends Component{
+    state={
+
+    }
+    render() {
+        return (
+          <div className='container'>
+            <div className="row">
+                <h1>Hey There</h1>
+            </div>
+          </div>
+        );
+      }
+}
+
+
+    
+
+
+
+
+
+export default UsersPage;
