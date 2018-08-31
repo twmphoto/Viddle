4c9bccd230375e7bc0d31c71c599736910eb8119
John Chu <twmphoto@icloud.com>
2018-08-31 11:33:22 -0700

    Cleanup 11:32AM

--- src/App.js
+++ src/App.js
@@ -8,15 +8,7 @@ import { LOGIN, HOME, USERS } from './constants/constants';
 
 class App extends Component {
 //Add Change 8/31 9:52AM
-  state = { 
-    path: LOGIN,
-    user: {
-      username: 'abc',
-      password: 'abc'
-    }
-  }
-
-  state = {
+state = {
     path: HOME,
     user: {
       username: 'abc',
@@ -48,17 +40,17 @@ login = user => {
   }
 }
 
-buttonClicked = () => {
-  this.props.login(this.state)
-  this.props.changePath(DASHBOARD)
-}
+// buttonClicked = () => {
+//   this.props.login(this.state)
+//   this.props.changePath(DASHBOARD)
+// }
 
 
   //Login/Create Account 
-  buttonClicked = () => {
-    this.props.login(this.state)
-    this.props.changePath(USERPAGE)
-  }
+  // buttonClicked = () => {
+  //   this.props.login(this.state)
+  //   this.props.changePath(USERPAGE)
+  // }
 
 
 
@@ -83,17 +75,17 @@ buttonClicked = () => {
   */
 
   //Add users
-  addUser = User => {
-    newUser = [...this.state.users, user];
-    this.setState({ users: NewUsers });
-  }
+  // addUser = User => {
+  //   newUser = [...this.state.users, user];
+  //   this.setState({ users: NewUsers });
+  // }
 
 
   /**
   //Black Box Delete Component, currently adapted for changing video collection
   // deleteVideo = index => {
   //   let newVideoCollection = [
-  //     ...this.state.users.vidcollection.slice(0, index/*What is this? */)
+  //     ...this.state.users.vidcollection.slice(0, index/*What is this?) */
   // ...this.state.users.vidcollection.slice(index + 1),
   //   ];
   //   this.setState({ users.vidcollection: /*what is this?*/ newVideoCollection })
@@ -182,35 +174,18 @@ buttonClicked = () => {
         <div><h3>Collect</h3></div>
         <div><h3>Display</h3></div>
         <div><h3>Share</h3></div>
-        <div><p>Collect all your favorite videos from your favorite media sites into one
-          seamless user experience</p></div>
-        <div><p>Post your latest video find to your personal wall.</p></div>
-        <div><p>Share your interests with the world, and meet new friends</p></div>
-
-      </div>
-      </div>
-      
-
-
-
-        <div>
-          <h3>Display</h3>
-        </div>
-        <div>
-          <h3>Share</h3>
-        </div>
-        // <div>
-        //   <p>Collect all your favorite videos from your favorite media sites into one
-        //   seamless user experience</p>
-        // </div>
-        // <div>
-        //   <p>Post your latest video find to your personal wall.</p>
-        // </div>
-        // <div>
-        //   <p>Share your interests with the world, and meet new friends</p>
-        // </div>
+          <div>
+           <p>Collect all your favorite videos from your favorite media sites into one
+           seamless user experience</p>
+           </div>
+          <div>
+           <p>Post your latest video find to your personal wall.</p>
+          </div>
+          <div>
+           <p>Share your interests with the world, and meet new friends</p>
+          </div>
     </div>
-  
+  </div>
     );
   }  
 }
